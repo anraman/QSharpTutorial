06195231ca8cc502ed9e9cb45646eaedce27a0e7
REDMOND\johnaz <johnaz@microsoft.com>
2018-04-19 10:32:52 +0100

    Point to Tutorial

--- Entanglement/Driver.cs
+++ Entanglement/Driver.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Entanglement.Reference
+namespace Entanglement
 {
     #region Hidden
     internal static class Utilities
--- SuperPosition/Driver.cs
+++ SuperPosition/Driver.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SuperPosition.Reference
+namespace SuperPosition
 {
 #region Hidden
     internal static class Utilities
